7cee52a94f1abb917406b72598e0ce07564341b7
aquesidilly <aquesidilly@yahoo.com>
2021-04-11 09:23:04 +0000

    PY-Milestone

--- main.py/app.py
+++ main.py/app.py
@@ -10,7 +10,7 @@ import os
 
 app = Flask(__name__)
 # app.config['MONGO_URI'] = 'mongodb://localhost:27017/movie'
-app.config['Kukuabaawa'] = os.environ.get("Kukua")
+app.config['MONGODB_URI'] = os.environ.get("MONGODB_URI")
 app.config.from_object(Config)
 
 mongo = PyMongo(app)
@@ -21,7 +21,7 @@ mongo = PyMongo(app)
 def index():
     """Home page the gets 4 movies from DB that have been viewed the most"""
     four_movies = mongo.db.movies.find().sort([('views', DESCENDING)]).limit(4)
-    return render_template('index.html', title="Home", movies=four_movies)
+    return render_template('index.html', title="Sign in", movies=four_movies)
 
 
 @app.route('/login', methods=['GET', 'POST'])
