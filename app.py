0a9cb2e892e8a3ecf1ca690ff571dc17b24be813
aquesidilly <aquesidilly@yahoo.com>
2021-04-12 18:58:09 +0000

    PY-Milestone

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def login():
         if session['logged_in'] is True:
             return redirect(url_for('index', title="Sign In"))
 
-    form = LoginForm(1)
+    form = LoginForm()
 
     if form.validate_on_submit():
         # get all users
