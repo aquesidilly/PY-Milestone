1e3650496946752872ec4c12d1ddc5256c437e1c
aquesidilly <aquesidilly@yahoo.com>
2021-04-14 17:45:58 +0000

    PY-Milestone

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('0123456') or '01234567'
-mongo "mongodb+srv://aquesidilly.yjxkk.mongodb.net/myFirstDatabase" --username PY-aquesidilly--password Nanaosei
+mongodb+srv://PY-aquesidilly:Love19901@cluster0.xmxi1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
