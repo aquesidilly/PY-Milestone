deedb095645e8da6bd97bbdc152c25acbd125c7b
aquesidilly <aquesidilly@yahoo.com>
2021-04-15 18:25:12 +0000

    PY-Milestone

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-class Config(object):
-    SECRET_KEY = os.environ.get('WYrd3mUMyl') or 'WYrd3mUMyl'
-mongodb+srv://PY-aquesidilly:Love19901@cluster0.xmxi1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
