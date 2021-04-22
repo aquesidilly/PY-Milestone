d280788c7ef1c62bca44c81d0f57d58a9c7bc700
aquesidilly <aquesidilly@yahoo.com>
2021-04-22 18:25:13 +0000

    PY-Milestone

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 os.environ.setdefault("IP","0.0.0.0")
 os.environ.setdefault("PORT","5000")
 os.environ.setdefault("SECRET_KEY","WYrd3mUMyl")
-os.environ.setdefault("MONGO_URI","mongodb+srv://PY-aquesidilly:Love19901@cluster0.xmxi1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_URI","mongodb+srv://PY-aquesidilly:Love19901@cluster0.xmxi1.mongodb.net/Dilly?retryWrites=true&w=majority")
 os.environ.setdefault("MONGO_DBNAME","aquesidilly-py-milestone")
